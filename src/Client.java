b5b4e06bbaca1e669eba73eeae9127ab37186b3a
ChauQuang1111 <148325990+ChauQuang1111@users.noreply.github.com>
2024-05-21 15:12:24 +0700

    first commit

---
+++ src/Client.java
@@ -0,0 +1,10 @@
+public class Client {
+    public static void main(String[] args) {
+        IDocument normalDoc = new NormalDoc();
+        normalDoc.SetEncryption("Normal Encryption");
+        normalDoc.SetExtension("Normal Extension");
+        Document normalDocument = normalDoc.buildDoc();
+        System.out.println("Normal Document: " + normalDocument.getEncryption() + " " + normalDocument.getExtension());
+
+    }
+}
